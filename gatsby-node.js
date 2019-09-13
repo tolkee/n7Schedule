94efebaccee43525f07f86ac8829f8aa04537bf5
atoOm3 <ato0m3.py@gmail.com>
2019-09-13 13:34:22 +0200

    Make the ics request during gatsby build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,30 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const axios = require('axios');
+const ical2json = require('ical2json');
+const fs = require('fs');
 
-// You can delete this file if you're not using it
+
+// Request the file ics, convert it to json and store it in data folder
+exports.onPreBuild = () => {
+    // empty /public folder
+    console.log("----------- Request the ICAL");
+    axios.get('https://calendar.google.com/calendar/ical/guillaume.lacoste65%40gmail.com/private-4580377d5b9948bdec093524a0ce9e59/basic.ics').then(
+    function (response) {
+        console.log("----------- No error on the request");
+        const text = response.data; 
+        console.log("----------- convert to JSON the ICAL");
+        const json = ical2json.convert(text);
+        console.log("----------- create JSON file");
+        fs.writeFile("./src/data/calendar.json", JSON.stringify(json), 'utf8', function (err) {
+            if (err) {
+                console.log("An error occured while writing JSON Object to File.");
+                return console.log(err);
+            }
+            console.log("JSON file has been saved.");
+        });
+    }
+    )
+    
+};
